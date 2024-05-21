a1c2801061aba7e12ad389fcace0fa5d2a74a297
Azhar <azharshaikh33@gmail.com>
2024-05-21 22:49:09 +0400

    Auto Push

--- docdb.tf
+++ docdb.tf
@@ -1,16 +1,18 @@
-# resource "aws_docdb_cluster" "docdb" {
-#   cluster_identifier = "roboshop-${var.ENV}-docdb"
-# }
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier = "roboshop-${var.ENV}-docdb"
+}
 
-# resource "aws_docdb_cluster" "docdb" {
-#   cluster_identifier      = "roboshop-${var.ENV}-docdb"
-#   engine                  = "docdb"
-#   master_username         = "admin1"
-#   master_password         = "roboshop1"
-# #   backup_retention_period = 5
-# #   preferred_backup_window = "07:00-09:00"
-#   skip_final_snapshot     = true
-# }
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "roboshop-${var.ENV}-docdb"
+  engine                  = "docdb"
+  master_username         = "admin1"
+  master_password         = "roboshop1"
+#   backup_retention_period = 5
+#   preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+  db_subnet_group_name    = aws_docdb_subnet_group.docdb_subnet_group.name
+
+}
 
 resource "aws_docdb_subnet_group" "docdb_subnet_group" {
   name       = "roboshop-${var.ENV}-docdb"
