d934a9c3ea5cfb0da6ec466d8febc82acc84c7d4
Azhar <azharshaikh33@gmail.com>
2024-05-26 04:18:58 +0400

    Auto Push

--- docdb.tf
+++ docdb.tf
@@ -24,8 +24,8 @@ resource "aws_docdb_subnet_group" "docdb_subnet_group" {
 }
 
 resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = 1
+  count              = var.DOCDB_INSTANCE_COUNT
   identifier         = "roboshop-${var.ENV}-docdb-instance"
   cluster_identifier = aws_docdb_cluster.docdb.id
-  instance_class     = "db.t3.medium"
+  instance_class     = var.DOCDB_INSTANCE_CLASS
 }
--- r53.tf
+++ r53.tf
@@ -1,7 +1,7 @@
-# resource "aws_route53_record" "mongodb" {
-#     zone_id = "Z093296193U6R55S5C7L"
-#     name    = "www.example.com"
-#     type    = "A"
-#     ttl     = 300
-#     records = [aws_eip.lb.public_ip]
-# }
+resource "aws_route53_record" "mongodb" {
+    zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+    name    = "mongodb.${var.ENV}"
+    type    = "CNAME"
+    ttl     = 10
+    records = [aws_docdb_cluster.docdb.endpoint]
+}
