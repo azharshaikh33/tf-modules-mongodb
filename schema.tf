d278c68ae5e4ffc183ac15103e0c2d33d0047fe0
Azhar <azharshaikh33@gmail.com>
2024-06-05 23:17:48 +0400

    Auto Push

--- schema.tf
+++ schema.tf
@@ -10,8 +10,8 @@ resource "null_resource" "schema" {
     curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
     unzip -o mongodb.zip
     cd mongodb-main
-    mongo --tls --host ${aws_docdb_cluster.docdb.endpoint} --tlsCAFile global-bundle.pem --username admin1 --password roboshop1 < catalogue.js
-    mongo --tls --host ${aws_docdb_cluster.docdb.endpoint} --tlsCAFile global-bundle.pem --username admin1 --password roboshop1 < users.js
+    mongo --tls --host ${aws_docdb_cluster.docdb.endpoint} --tlsCAFile /tmp/global-bundle.pem --username admin1 --password roboshop1 < catalogue.js
+    mongo --tls --host ${aws_docdb_cluster.docdb.endpoint} --tlsCAFile /tmp/global-bundle.pem --username admin1 --password roboshop1 < users.js
     EOF
   }
 }
