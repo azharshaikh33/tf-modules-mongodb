7761352b2a1625727a3db58493c3558d4dee95f9
Azhar <azharshaikh33@gmail.com>
2024-06-05 12:37:32 +0400

    Auto Push

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 # creating locals
 
 locals {
-  DOCDB_USER = jsondecode(aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_USERNAME"]
-  DOCDB_PASS = jsondecode(aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_PASSWORD"]
+  DOCDB_USER = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_USERNAME"]
+  DOCDB_PASS = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_PASSWORD"]
 }
