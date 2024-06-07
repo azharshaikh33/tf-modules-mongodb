c2557fd7fcaca044bf613ccd7ce35bfbd8bdf962
Azhar <azharshaikh33@gmail.com>
2024-06-07 09:28:48 +0400

    Auto Push

--- schema.tf
+++ schema.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "schema" {
 
-    depends_on = [aws_docdb_cluster.docdb]
+    depends_on = [aws_docdb_cluster.docdb ,aws_docdb_cluster_instance.cluster_instances]
 
   provisioner "local-exec" {
     command = <<EOF
